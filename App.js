55984fcbe389f781238dc748049fc78198709741
vikrantnegi <vikrantnegi007@gmail.com>
2019-02-20 11:02:06 +0530

    upgrade react native to 0.58.4

--- App.js
+++ App.js
@@ -4,11 +4,17 @@
  *
  * @format
  * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 import SearchableFlatList from './src/SearchableList';
 
-const App = () => <SearchableFlatList />;
+const App = () => (
+  <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+    <SearchableFlatList />
+  </SafeAreaView>
+);
 
 export default App;
